d9bf4ef94ff81947e394b3dd6bb348a7a129a38a
pablo361 <pizarro993@gmail.com>
2023-06-18 11:17:42 -0300

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Pablo Pizarro on 18/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
